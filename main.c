4f621bf180e527a2bdd6dada6bf79127fc0e8ea1
Connor Hallett <connor@iouconcepts.com>
2016-02-03 10:38:25 -0500

    Added C files

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+
+#include <mpi.h>
+
+int main(int argc, char** argv)
+{
+
+  int my_rank, p;
+  int i, dest, source;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &p);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+
+  MPI_Status status;
+
+
+
+  MPI_Finalize();
+  return(0);
+}
