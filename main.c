44651be3623a1fe1730311f1047cb7c7345322ea
Connor Hallett <connor@iouconcepts.com>
2016-02-03 16:58:55 -0500

    Implementation using ibcast, not working locally, to test on mpihost

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <math.h>
 #include <assert.h>
+#include <time.h>
 
 #include <gmp.h>
 #include <mpi.h>
@@ -16,7 +17,9 @@ int main(int argc, char** argv)
     mpz_t currentPrime;
     unsigned long int product = atoi(argv[1]);
     int secondFactor = 0;
-    int iterations =0;
+    int iterations = 0;
+    int bcastStatus;
+    
     
     mpz_t N;
     mpz_t nextPrimeNumber;
@@ -25,7 +28,9 @@ int main(int argc, char** argv)
     mpz_init_set_ui(currentPrime, 2);
     mpz_nextprime(nextPrimeNumber, N);
     
-    printf("N = %s , nextPrime: %d \n", mpz_get_str(NULL, 10, N), mpz_get_ui(nextPrimeNumber));
+    MPI_Request finalValue;
+    
+    //printf("N = %s , nextPrime: %d \n", mpz_get_str(NULL, 10, N), mpz_get_ui(nextPrimeNumber));
     
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
@@ -34,7 +39,14 @@ int main(int argc, char** argv)
 
   MPI_Status status;
     
-    while (!doneFlag) {
+    clock_t start = clock(), diff;
+    if (my_rank == 0) {
+        MPI_Irecv(&secondFactor, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    } else {
+        bcastStatus = MPI_Ibcast(&secondFactor, 1, MPI_INT, 0, MPI_COMM_WORLD, &finalValue);
+    }
+    
+    while (!secondFactor) {
         if (!iterations) {
             iterations++;
             for (i=0 ; i < my_rank ; i++) {
@@ -46,21 +58,35 @@ int main(int argc, char** argv)
             }
         }
         
-        printf("currentPrime for %d = %d \n",my_rank,  mpz_get_ui(currentPrime));
-        printf("product \% mpz_get_ui(currentPrime) = %d \n", product % mpz_get_ui(currentPrime));
+        //printf("currentPrime for %d = %d \n",my_rank,  mpz_get_ui(currentPrime));
+        //printf("product \% mpz_get_ui(currentPrime) = %d \n", product % mpz_get_ui(currentPrime));
         if (product % mpz_get_ui(currentPrime) == 0) {
             doneFlag = 1;
             secondFactor = product / mpz_get_ui(currentPrime);
             printf("done by process %d, factors are %d and %d \n", my_rank, mpz_get_ui(currentPrime), secondFactor);
+            MPI_Send(&secondFactor, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD);
+
         }
-        MPI_Allreduce(
-                    &doneFlag,
-                    &doneFlag,
-                    1,
-                    MPI_INT,
-                    MPI_MAX,
-                    MPI_COMM_WORLD);
-        
+//        MPI_Allreduce(
+//                    &secondFactor,
+//                    &secondFactor,
+//                    1,
+//                    MPI_INT,
+//                    MPI_MAX,
+//                    MPI_COMM_WORLD);
+    }
+    diff = clock() - start;
+    if (my_rank == 0) {
+        bcastStatus =MPI_Ibcast(&secondFactor, 1, MPI_INT, 0, MPI_COMM_WORLD, &finalValue);
+    }
+    
+    int msec = diff * 1000 / CLOCKS_PER_SEC;
+    printf("Time taken %d seconds %d milliseconds by process %d \n", msec/1000, msec%1000, my_rank);
+    
+    if (my_rank == 0) {
+        MPI_Ibcast(&secondFactor, 1, MPI_INT, 0, MPI_COMM_WORLD, &finalValue);
+    } else {
+    
     }
 
 
